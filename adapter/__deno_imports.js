ae3221fa7b5e9dab8df96cf44789bd9b05835c0e
Benjamin Haines <bhaines@yext.com>
2023-10-23 23:09:54 -0400

    remove tailwind

--- adapter/__deno_imports.js
+++ adapter/__deno_imports.js
@@ -2,8 +2,6 @@
 // It will be replaced in the final Deno build.
 //
 // This allows us to prerender pages in Node.
-import { path } from "https://deno.land/x/ramda/index.js";
-
 export class Server {
 	listenAndServe() {}
 }
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import yext from "./adapter/index";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: yext(),
-  integrations: [tailwind()]
+  adapter: yext()
 });
